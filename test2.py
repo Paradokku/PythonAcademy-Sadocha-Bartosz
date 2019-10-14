d797b43ace938a798b5e19f1bd4a69720d097dc0
Bartosz Sadocha <sadocha.bartosz@gmail.com>
2019-10-14 19:00:54 +0200

    Tests

---
+++ test2.py
@@ -0,0 +1,16 @@
+my_height = float(1.87)
+my_name = str("Sadocha Bartosz")
+my_age = int(19)
+
+print(my_height,"m")
+
+class Sadocha:
+    pass
+
+class Bartek(Sadocha):
+    pass
+
+this_is_bartek = Bartek()
+print(this_is_bartek)
+
+
